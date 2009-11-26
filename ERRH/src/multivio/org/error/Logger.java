6ba0b966616f1d9e7a9e95f1581241c46594cffb
fca <francesco.carrino@hefr.ch>
2009-11-26 11:26:18 +0100

    MVO_ERRH: add information about client IP address and fix some bug in Logger.java

--- ERRH/src/multivio/org/error/Logger.java
+++ ERRH/src/multivio/org/error/Logger.java
@@ -3,94 +3,65 @@ package multivio.org.error;
 import java.io.*;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import javax.servlet.http.*;
 
 /**
- * 
- * This class reads information from the message sent to the server and extracts
- * and saves in a file the relevant fields such as: user name, level, message,
- * user’s message, etc. Moreover, it adds a timestamp referred to server-side
- * time.
- * 
+ *
+ * @class Logger.java reads information from the message sent to the server and extracts
+ *               and saves in a file the relevant fields such as: client ip address, level, message,
+ *               user’s message, etc. Moreover, it adds a timestamp referred to server-side time.
  * @author FCA
- * @version {0.1.0}
+ * @ version {0.9.0}
+ *
  */
-
 public class Logger {
-  
-  private static final String FILENAME = "../webapps/zircon/log.txt";
-  /* keywords to search for in the message */
-  private static final String[] KEYTAB = { "level", "\"message", "\'message", "useragent", "custom message" };
-  private static final String regex = "[\"\',]";
-
+  public final String FILENAME = "../webapps/zircon/log.txt";
   public String add (HttpServletRequest request, HttpServletResponse response) throws IOException {
-    Pattern p;
-    Matcher m;
-    String line, tmpLine = "";
-    int contentLen, length = 0;
-
-    String clientName = request.getParameter("ClientName");
-    if (clientName != null) {
-      writeLog("\nClient Name: " + clientName + "\n");
+    
+    String message = "";
+    int length = 1;    
+    
+    String clientIP = request.getRemoteAddr() ;
+    if (clientIP != null) {
+    message += "\nClientIP: " + clientIP + "\n";
     }
-
+  
     Calendar calendar = new GregorianCalendar();
-    int hours = calendar.get(Calendar.HOUR_OF_DAY);
-    int minutes = calendar.get(Calendar.MINUTE);
-    int seconds = calendar.get(Calendar.SECOND);
-    int day = calendar.get(Calendar.DAY_OF_MONTH);
-    /* 1 is necessary to have a correct visualization */
-    int moth = calendar.get(Calendar.MONTH) + 1;
-    int year = calendar.get(Calendar.YEAR);
-    String time = day + "/" + moth + "/" + year + " " + hours + ":" + minutes + ":" + seconds;
-    if (time != null) writeLog(time + "\n");
-
-    contentLen = request.getContentLength();
+    String time = calendar.getTime().toString();
+    message += time + "\n";
+    
+    int contentLen = request.getContentLength();
     if (contentLen > 0) {
-      byte buffer[] = new byte[contentLen];
-      length = request.getInputStream().readLine(buffer, 0, buffer.length);
       while (length > 0) {
-        line = new String(buffer, 0, length);
-
-        for (int i = 0; i < KEYTAB.length; i++) {
-          p = Pattern.compile(KEYTAB[i]);
-          m = p.matcher(line);
-          if (m.find()) {
-            tmpLine = line.substring(m.end());
-            // Check if the pattern is "\'message"
-            line = (i == 2) ? "EXCEPTION TYPE" + tmpLine :
-                KEYTAB[i].toUpperCase() + tmpLine;
-            line = line.replaceAll(regex, "");
-            writeLog(line);
-            break;
-          }
-        }
-        buffer = new byte[contentLen];
-        length = request.getInputStream().readLine(buffer, 0, buffer.length);
+        byte buffer[] = new byte[2048];
+        length = request.getInputStream().readLine (buffer, 0, buffer.length);
+        if (length > 0) {
+          message += new String (buffer, 0, length);
+        } 
       }
     }
-    return new String("one log has been added");
+    writeLog(message + "\n");
+    return new String ("one log has been added");
   }
-
+ 
   /**
+   * @method 
    * writeLog writes the log information in a .txt file.
-   * 
    * @param value information to be add in the log file.
+   *
    */
   private void writeLog (String value) {
     PrintWriter p;
     try {
       // Open Stream writer
-      p = new PrintWriter(new BufferedWriter(new FileWriter(FILENAME, true)));
+      p = new PrintWriter (new BufferedWriter (new FileWriter (FILENAME, true)));
       // Write File
-      p.print(value);
+      p.print (value);
       // Close File
       p.close();
     } catch (IOException e) {
       e.printStackTrace();
     }
   }
-
+  
 }
